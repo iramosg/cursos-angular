470dc262a3ecad18a4ba0525b58c7bd0e60bbca6
Igor Ramos <igor.ramos@live.com>
2023-10-28 11:35:22 -0300

    Aula 15

--- curso-angular-v15/meu-primeiro-projeto/src/app/app.component.ts
+++ curso-angular-v15/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log(1);
+    }, 5000);
+  }
+}
