91291a67b828251ba6e319a40b81905dcd050728
Igor Ramos <igor.ramos@live.com>
2023-10-28 11:25:14 -0300

    Aula 12

--- curso-angular-v15/meu-primeiro-projeto/src/app/title/title.component.ts
+++ curso-angular-v15/meu-primeiro-projeto/src/app/title/title.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss'],
 })
 export class TitleComponent {
-
+  public title: string = 'Bem-vindo!';
 }
