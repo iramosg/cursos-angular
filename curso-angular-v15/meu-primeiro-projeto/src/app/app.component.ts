da3e3b40d3fd0b11ad3afb423d467e3d2f37e18e
Igor Ramos <igor.ramos@live.com>
2023-10-28 11:55:39 -0300

    Aula 17

--- curso-angular-v15/meu-primeiro-projeto/src/app/app.component.ts
+++ curso-angular-v15/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,19 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
+    {{ valor }}
+    <button (click)="adicionar()">Adicionar</button>
     <app-title title="Olá, mundo!"></app-title>
-    <app-title></app-title>
-    <router-outlet></router-outlet>
   `,
 })
-export class AppComponent implements OnInit {
+export class AppComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  public valor: number = 1;
+
   constructor() {}
+
+  public adicionar(): number {
+    return (this.valor += 1);
+  }
 
   ngOnInit(): void {
     setTimeout(() => {
       console.log(1);
     }, 5000);
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
   }
 }
