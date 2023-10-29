7ae558eb4739dacd06144316f1c9e963f51018f9
Igor Ramos <igor.ramos@live.com>
2023-10-29 12:14:47 -0300

    Início lógica todo-list

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
-
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova task', checked: true },
+    { task: 'Minha nova task 2', checked: false },
+  ];
 }
--- app-todo/src/app/modules/home/home.module.ts
+++ app-todo/src/app/modules/home/home.module.ts
@@ -9,6 +9,7 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 // Page
 import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,6 @@ import { HomeComponent } from './pages/home/home.component';
     TodoListComponent,
     HomeComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class HomeModule {}
