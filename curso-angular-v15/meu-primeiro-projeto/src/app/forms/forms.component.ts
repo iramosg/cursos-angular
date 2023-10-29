094a588b6054e2f556513de33170008482a98540
Igor Ramos <igor.ramos@live.com>
2023-10-29 18:52:35 -0300

    Aula 65

--- curso-angular-v15/meu-primeiro-projeto/src/app/forms/forms.component.ts
+++ curso-angular-v15/meu-primeiro-projeto/src/app/forms/forms.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent {
-
+  public list: Array<{ comida: string }> = [
+    { comida: 'X-Salada' },
+    { comida: 'X-Bacon' },
+    { comida: 'Coxinha' },
+  ];
 }
