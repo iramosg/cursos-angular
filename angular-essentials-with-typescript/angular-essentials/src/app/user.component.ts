2311c2287f0d7f15c5e1698926c1ea69a16dd7a0
Igor Ramos <igor.ramos@live.com>
2023-10-31 20:34:20 -0300

    Aula 26

--- angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
+++ angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     <!-- <input type="text" [(ngModel)]="name" /> -->
     <p>Hello, {{ name }}!</p>
     <p>I'm the user component</p>
+    <app-user-detail></app-user-detail>
   `,
 })
 export class UserComponent {
