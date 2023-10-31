d1b2b9f9c61b392862e8a66e347206fc4cca8c5c
Igor Ramos <igor.ramos@live.com>
2023-10-31 11:58:32 -0300

    Aula 18 e 19

---
+++ angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Hello, {{ name }}!</p>
+    <p>I'm the user component</p>
+  `,
+})
+export class UserComponent {
+  name = 'Igor';
+}
