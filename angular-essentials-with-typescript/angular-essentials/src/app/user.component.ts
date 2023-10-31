b5ef5232da53a72273118baaa475e972e670f506
Igor Ramos <igor.ramos@live.com>
2023-10-31 12:05:16 -0300

    Aula 21

--- angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
+++ angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" (input)="onUserInput($event)" />
+    <input type="text" (input)="onUserInput($event)" [value]="name" />
     <p>Hello, {{ name }}!</p>
     <p>I'm the user component</p>
   `,
