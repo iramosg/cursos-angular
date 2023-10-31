95b3b031703ceb62cd8591f96ec84bc78912caf4
Igor Ramos <igor.ramos@live.com>
2023-10-31 20:31:23 -0300

    Aula 25

--- angular-essentials-with-typescript/angular-essentials/src/app/app.component.ts
+++ angular-essentials-with-typescript/angular-essentials/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-essentials';
+  rootName = 'Igor';
+  onNameChanged(newName: string) {
+    this.rootName = newName;
+  }
 }
--- angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
+++ angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" [(ngModel)]="name" />
+    <input type="text" (input)="onUserInput($event)" [value]="name" />
+    <!-- <input type="text" [(ngModel)]="name" /> -->
     <p>Hello, {{ name }}!</p>
     <p>I'm the user component</p>
   `,
 })
 export class UserComponent {
   @Input() name: any;
+  @Output() nameChanged = new EventEmitter<string>();
 
   onUserInput(event: any) {
-    this.name = event.target.value;
+    // this.name = event.target.value;
+    this.nameChanged.emit(event.target.value);
   }
 }
