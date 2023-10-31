fcaee40cfacb8f30c372127985912311a125d881
Igor Ramos <igor.ramos@live.com>
2023-10-31 12:02:13 -0300

    Aula 20

--- angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
+++ angular-essentials-with-typescript/angular-essentials/src/app/user.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
+    <input type="text" (input)="onUserInput($event)" />
     <p>Hello, {{ name }}!</p>
     <p>I'm the user component</p>
   `,
 })
 export class UserComponent {
   name = 'Igor';
+
+  onUserInput(event: any) {
+    this.name = event.target.value;
+  }
 }
