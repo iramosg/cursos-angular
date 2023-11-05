be87bb47a4e400667c8cfea3238b9a972752603e
Igor Ramos <igor.ramos@live.com>
2023-11-05 20:25:03 -0300

    Aula 87

--- angular-essentials-with-typescript/project-01/src/app/tabs/tabs.component.ts
+++ angular-essentials-with-typescript/project-01/src/app/tabs/tabs.component.ts
@@ -7,20 +7,17 @@ import { StarWarsService } from '../star-wars.service';
   styleUrls: ['./tabs.component.css'],
 })
 export class TabsComponent {
-  characters: any = [];
-  chosenList = 'all';
-  swService: StarWarsService;
-
-  constructor(swService: StarWarsService) {
-    this.swService = swService;
-  }
-
-  onChoose(side: string) {
-    this.chosenList = side;
-  }
-
-  getCharacters() {
-    this.characters = this.swService.getCharacters(this.chosenList);
-    return this.characters;
-  }
+  // characters: any = [];
+  // chosenList = 'all';
+  // swService: StarWarsService;
+  // constructor(swService: StarWarsService) {
+  //   this.swService = swService;
+  // }
+  // onChoose(side: string) {
+  //   this.chosenList = side;
+  // }
+  // getCharacters() {
+  //   this.characters = this.swService.getCharacters(this.chosenList);
+  //   return this.characters;
+  // }
 }
