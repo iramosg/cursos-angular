8177cbf664f7bf4acb4949ebc09d19b39090bbca
Igor Ramos <igor.ramos@live.com>
2023-11-05 15:23:09 -0300

    Aula 49

---
+++ angular-essentials-with-typescript/project-01/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
