84880b33501c5ac70422665b22e30a9f79c5295a
Igor Ramos <igor.ramos@live.com>
2023-11-05 20:16:22 -0300

    Aula 85

--- angular-essentials-with-typescript/project-01/src/app/list/list.component.ts
+++ angular-essentials-with-typescript/project-01/src/app/list/list.component.ts
@@ -1,15 +1,29 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {
-  @Input() characters: any;
+export class ListComponent implements OnInit {
+  characters: any = [];
+  activateRoute: ActivatedRoute;
+  swService: StarWarsService;
   // @Output() sideAssigned = new EventEmitter<{ name: string; side: string }>();
 
   // onSideAssigned(charInfo: any) {
   //   this.sideAssigned.emit(charInfo);
   // }
+
+  constructor(activateRoute: ActivatedRoute, swService: StarWarsService) {
+    this.activateRoute = activateRoute;
+    this.swService = swService;
+  }
+  ngOnInit() {
+    this.activateRoute.params.subscribe((params: any) => {
+      this.characters = this.swService.getCharacters(params.side);
+    });
+  }
 }
