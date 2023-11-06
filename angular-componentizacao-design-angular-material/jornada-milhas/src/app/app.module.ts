19469cba15b35eb84d6c8499eb6c6b7eb0f85c9c
Igor Ramos <igor.ramos@live.com>
2023-11-06 13:40:41 -0300

    Aula 03-05

--- angular-componentizacao-design-angular-material/jornada-milhas/src/app/app.module.ts
+++ angular-componentizacao-design-angular-material/jornada-milhas/src/app/app.module.ts
@@ -15,6 +15,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
 import { MatCardModule } from '@angular/material/card';
 import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
+import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -27,6 +33,7 @@ import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoiment
     HomeComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
+    FormBuscaComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,6 +42,11 @@ import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoiment
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
