78f57fc68c86ca9c3fb0bfb00cfe11a671d044c9
Igor Ramos <igor.ramos@live.com>
2023-11-05 21:01:17 -0300

    Aula 94

--- angular-essentials-with-typescript/project-01/src/app/list/list.component.ts
+++ angular-essentials-with-typescript/project-01/src/app/list/list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StarWarsService } from '../star-wars.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   characters: any = [];
   activateRoute: ActivatedRoute;
   swService: StarWarsService;
   loadedSide = 'all';
+  subscription: any;
   // @Output() sideAssigned = new EventEmitter<{ name: string; side: string }>();
 
   // onSideAssigned(charInfo: any) {
@@ -22,14 +24,19 @@ export class ListComponent implements OnInit {
     this.activateRoute = activateRoute;
     this.swService = swService;
   }
+
   ngOnInit() {
     this.activateRoute.params.subscribe((params: any) => {
       this.characters = this.swService.getCharacters(params.side);
       this.loadedSide = params.side;
     });
 
-    this.swService.charactersChanged.subscribe(() => {
+    this.subscription = this.swService.charactersChanged.subscribe(() => {
       this.characters = this.swService.getCharacters(this.loadedSide);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
