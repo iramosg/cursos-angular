f42eb27130fb7a4aa691a19845e79ea82e33f53f
Igor Ramos <igor.ramos@live.com>
2023-11-06 12:18:05 -0300

    Aula 02-02

--- angular-componentizacao-design-angular-material/jornada-milhas/src/app/shared/banner/banner.component.ts
+++ angular-componentizacao-design-angular-material/jornada-milhas/src/app/shared/banner/banner.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent {
-
+  @Input() src: string = '';
+  @Input() alt: string = '';
 }
