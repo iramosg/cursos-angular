a18f995ff4b612d0c063978e3839fc65139ea0b6
Igor Ramos <igor.ramos@live.com>
2023-11-07 13:01:45 -0300

    Refatorando open dialog

--- desenvolva-aplicacoes-escalaveis-com-angular/jornada-milhas/src/app/core/services/form-busca.service.ts
+++ desenvolva-aplicacoes-escalaveis-com-angular/jornada-milhas/src/app/core/services/form-busca.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +9,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class FormBuscaService {
   formBusca: FormGroup;
 
-  constructor() {
+  constructor(private dialog: MatDialog) {
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
@@ -21,5 +23,11 @@ export class FormBuscaService {
       throw new Error(`FormControl com nome "${nome}" não existe.`);
     }
     return control as FormControl;
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%',
+    });
   }
 }
--- desenvolva-aplicacoes-escalaveis-com-angular/jornada-milhas/src/app/shared/form-busca/form-busca.component.ts
+++ desenvolva-aplicacoes-escalaveis-com-angular/jornada-milhas/src/app/shared/form-busca/form-busca.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ModalComponent } from '../modal/modal.component';
 import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 
 @Component({
@@ -9,14 +7,5 @@ import { FormBuscaService } from 'src/app/core/services/form-busca.service';
   styleUrls: ['./form-busca.component.scss'],
 })
 export class FormBuscaComponent {
-  constructor(
-    public dialog: MatDialog,
-    public formBuscaService: FormBuscaService
-  ) {}
-
-  openDialog() {
-    this.dialog.open(ModalComponent, {
-      width: '50%',
-    });
-  }
+  constructor(public formBuscaService: FormBuscaService) {}
 }
