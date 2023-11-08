a1bdc054eaaead1f25013d1002b3c5a02ca2f093
Igor Ramos <igor.ramos@live.com>
2023-11-08 10:30:12 -0300

    Rodapé

--- angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app.module.ts
+++ angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
