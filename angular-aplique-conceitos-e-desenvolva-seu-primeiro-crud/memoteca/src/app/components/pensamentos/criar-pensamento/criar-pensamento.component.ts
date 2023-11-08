f4bb9255c4bbc3541f385a55b1334473f9dd3ae0
Igor Ramos <igor.ramos@live.com>
2023-11-08 15:44:51 -0300

    Exibindo mensagens de erro

--- angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -36,7 +36,7 @@ export class CriarPensamentoComponent implements OnInit {
   }
 
   criarPensamento() {
-    console.log(this.formulario.status);
+    console.log(this.formulario.get('autoria')?.errors);
     if (this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listar-pensamento']);
