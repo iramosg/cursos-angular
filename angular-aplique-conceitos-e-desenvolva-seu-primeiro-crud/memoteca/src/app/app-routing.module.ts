3847e860bcee4c073286196c5eeda16e45a7b92e
Igor Ramos <igor.ramos@live.com>
2023-11-08 11:09:24 -0300

    Configuração das rotas

--- angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app-routing.module.ts
+++ angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listar-pensamento', pathMatch: 'full' },
+  { path: 'listar-pensamento', component: ListarPensamentoComponent },
+  { path: 'criar-pensamento', component: CriarPensamentoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
