d0a7f95461d3df34fea605eb10639357305cbde8
Igor Ramos <igor.ramos@live.com>
2023-11-08 10:51:02 -0300

    Event binding

--- angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -12,4 +12,12 @@ export class CriarPensamentoComponent {
     autoria: 'Dev',
     modelo: '',
   };
+
+  criarPensamento() {
+    alert('Novo pensamento criado');
+  }
+
+  cancelar() {
+    alert('Ação cancelada');
+  }
 }
