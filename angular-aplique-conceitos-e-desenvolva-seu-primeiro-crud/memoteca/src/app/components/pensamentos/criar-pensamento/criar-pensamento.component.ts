9d2ecd8d4db1f8c0d4aa4aa75751b1f34ceb49e4
Igor Ramos <igor.ramos@live.com>
2023-11-08 10:45:54 -0300

    Property Binding

--- angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent {
-
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: '',
+  };
 }
