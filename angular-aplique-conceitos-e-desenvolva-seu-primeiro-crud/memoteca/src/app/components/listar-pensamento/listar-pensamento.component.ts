57cb88fe0211b04a64c7a86c178a54889bae4b56
Igor Ramos <igor.ramos@live.com>
2023-11-08 16:33:38 -0300

    Criando campo de busca

--- angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/listar-pensamento/listar-pensamento.component.ts
+++ angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/listar-pensamento/listar-pensamento.component.ts
@@ -11,6 +11,7 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = '';
 
   constructor(private service: PensamentoService) {}
 
