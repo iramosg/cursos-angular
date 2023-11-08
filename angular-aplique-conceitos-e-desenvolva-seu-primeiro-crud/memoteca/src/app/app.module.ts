cce820db051d90512088edfb9bdaf5c29101348e
Igor Ramos <igor.ramos@live.com>
2023-11-08 10:39:43 -0300

    Componente de formulário

--- angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app.module.ts
+++ angular-aplique-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
