ea6eafe46c5d2cda3e86736c20dce7f77bad0283
Igor Ramos <igor.ramos@live.com>
2023-11-09 13:06:43 -0300

    Exibindo os itens e chamada OnChanges

--- angular-ciclo-de-vida/lista-de-compras/src/app/components/item/item.component.ts
+++ angular-ciclo-de-vida/lista-de-compras/src/app/components/item/item.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit {
-
+  @Input() item!: Item;
   faPen = faPen;
-  faTrash = faTrash
+  faTrash = faTrash;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
