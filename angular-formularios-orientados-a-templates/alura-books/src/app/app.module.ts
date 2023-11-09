6e177e5be5c58a3bef5c3501048a3e0bd72ffbd0
Igor Ramos <igor.ramos@live.com>
2023-11-09 10:16:33 -0300

    Importando o FormsModule

---
+++ angular-formularios-orientados-a-templates/alura-books/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { SucessoCadastroComponent } from "./sucesso-cadastro/sucesso-cadastro.component";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CadastroComponent,
+    SucessoCadastroComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ angular-formularios-orientados-a-templates/alura-books/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,23 @@
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
+})
+export class CadastroComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  cadastrar(form: NgForm) {
+    if (form.valid) {
+      this.router.navigate(["/sucesso"]);
+    } else {
+      alert("Formulário inválido");
+    }
+    console.log(form);
+  }
+}
