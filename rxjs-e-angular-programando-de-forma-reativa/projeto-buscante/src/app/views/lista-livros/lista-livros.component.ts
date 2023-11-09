818ecf5ab9bdca060b93c49876a1b61832a968c4
Igor Ramos <igor.ramos@live.com>
2023-11-09 18:19:19 -0300

    Unsubscribe

--- rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,14 +7,19 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
   listaLivros: [];
   campoBusca: string = '';
+  subscription: Subscription;
 
   constructor(private service: LivroService) {}
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe({
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: (result) => console.log(result),
       error: (error) => console.log(error), // Se parar aqui, não executa o complete
       complete: () => console.log('Observable completado.'),
