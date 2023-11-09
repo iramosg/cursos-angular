bffd51cf8d6f1f4c38d66dcffa4af63d48821a8d
Igor Ramos <igor.ramos@live.com>
2023-11-09 13:43:44 -0300

    Criando a lista no localStorage

--- angular-ciclo-de-vida/lista-de-compras/src/app/service/lista-de-compra.service.ts
+++ angular-ciclo-de-vida/lista-de-compras/src/app/service/lista-de-compra.service.ts
@@ -5,29 +5,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ListaDeCompraService {
-  private listaDeCompra: Item[] = [
-    {
-      id: 1,
-      nome: 'Queijo prato',
-      data: 'Segunda-feira (31/10/2022) às 08:30',
-      comprado: false,
-    },
-    {
-      id: 2,
-      nome: 'Leite integral',
-      data: 'Segunda-feira (31/10/2022) às 08:30',
-      comprado: false,
-    },
-    {
-      id: 3,
-      nome: 'Mamão papaia',
-      data: 'Segunda-feira (31/10/2022) às 08:30',
-      comprado: true,
-    },
-  ];
+  private listaDeCompra: Item[];
 
   constructor() {
-    console.log('Instanciando dependências necessárias para o serviço.');
+    this.listaDeCompra = JSON.parse(localStorage.getItem('itens') || '[]');
   }
 
   getListaDeCompra() {
