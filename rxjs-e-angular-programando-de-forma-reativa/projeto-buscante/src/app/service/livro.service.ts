594b6adfa4573b8b762e8533b5a7d53d46034ae0
Igor Ramos <igor.ramos@live.com>
2023-11-09 18:28:18 -0300

    Lidando com a resposta da API

--- rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/service/livro.service.ts
+++ rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class LivroService {
 
   constructor(private http: HttpClient) {}
 
-  buscar(valorDigitado: string): Observable<any> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
     return this.http.get(this.api, { params });
   }
