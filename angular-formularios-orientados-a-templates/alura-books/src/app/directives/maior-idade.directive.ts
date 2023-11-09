c430aad4f9b8b14f74f45f51979aa64f50d1515a
Igor Ramos <igor.ramos@live.com>
2023-11-09 11:31:53 -0300

    Diretiva de validação

--- angular-formularios-orientados-a-templates/alura-books/src/app/directives/maior-idade.directive.ts
+++ angular-formularios-orientados-a-templates/alura-books/src/app/directives/maior-idade.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from "@angular/forms";
 
 @Directive({
-  selector: '[appMaiorIdade]'
+  selector: "[maiorIdadeValidator]",
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: MaiorIdadeDirective,
+      multi: true,
+    },
+  ],
 })
-export class MaiorIdadeDirective {
+export class MaiorIdadeDirective implements Validator {
+  constructor() {}
 
-  constructor() { }
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const dataNascimento = control.value;
+    const anoNascimento = new Date(dataNascimento).getFullYear();
+    const anoAtual = new Date().getFullYear();
+    const anoNascimentoMais18 = anoNascimento + 18;
+    const ehMaior = anoNascimentoMais18 <= anoAtual;
 
+    return ehMaior ? null : { maiorIdadeValidator: true };
+  }
 }
