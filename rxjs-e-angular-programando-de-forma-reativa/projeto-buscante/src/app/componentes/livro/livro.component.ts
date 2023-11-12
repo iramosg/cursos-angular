377cb4fde76f9ad8607683827a11f2131ee0e04f
Igor Ramos <igor.ramos@live.com>
2023-11-12 10:42:52 -0300

    Refatorando o código

--- rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/componentes/livro/livro.component.ts
+++ rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/componentes/livro/livro.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { Livro } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-livro',
   templateUrl: './livro.component.html',
-  styleUrls: ['./livro.component.css']
+  styleUrls: ['./livro.component.css'],
 })
 export class LivroComponent {
-
-  @Input() livro: Object;
+  @Input() livro: Livro;
   modalAberto: boolean;
 
   onModalChange(evento: boolean) {
--- rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -8,9 +10,10 @@ import { LivroService } from 'src/app/service/livro.service';
   styleUrls: ['./lista-livros.component.css'],
 })
 export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
   subscription: Subscription;
+  livro: Livro;
 
   constructor(private service: LivroService) {}
 
@@ -20,9 +23,17 @@ export class ListaLivrosComponent implements OnDestroy {
 
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (result) => console.log(result),
+      next: (result) => {
+        this.listaLivros = this.livrosResultadoParaLivros(result);
+      },
       error: (error) => console.log(error), // Se parar aqui, não executa o complete
       complete: () => console.log('Observable completado.'),
+    });
+  }
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
     });
   }
 }
