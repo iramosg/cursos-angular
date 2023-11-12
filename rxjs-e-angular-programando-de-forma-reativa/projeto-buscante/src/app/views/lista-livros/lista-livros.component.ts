73e798c9eac0098866f6f70c800c19012cf748c4
Igor Ramos <igor.ramos@live.com>
2023-11-12 12:36:17 -0300

    Refatoração

--- rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -27,38 +27,27 @@ export class ListaLivrosComponent {
   campoBusca = new FormControl();
   mensagemErro = '';
   livrosResultado: LivrosResultado;
+  listaLivros = [];
 
   constructor(private service: LivroService) {}
 
-  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
-    debounceTime(PAUSA),
-    filter((valorDigitado) => valorDigitado.length >= 3),
-    tap(() => console.log('Fluxo inicial')),
-    distinctUntilChanged(),
-    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map((resultado) => (this.livrosResultado = resultado)),
-    catchError((erro) => {
-      console.log(erro);
-      return of();
-    })
-  );
-
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA),
+    tap(() => {
+      console.log('Fluxo inicial de dados');
+    }),
     filter((valorDigitado) => valorDigitado.length >= 3),
-    tap(() => console.log('Fluxo inicial')),
-    distinctUntilChanged(),
-    switchMap((valorDigitado) => this.service.buscar(valorDigitado)), // Vai pegar o último valor digitado para enviar ao servidor
-    tap((result) => console.log('Requisição ao servidor', result)),
-    map((result) => result.items ?? []),
-    map((result) => this.livrosResultadoParaLivros(result)),
-    catchError(() => {
-      this.mensagemErro = 'Ops, ocorreu um erro. Recaregue a aplicação.';
-      // return EMPTY;
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((resultado) => (this.livrosResultado = resultado)),
+    map((resultado) => resultado.items ?? []),
+    tap(console.log),
+    map((items) => (this.listaLivros = this.livrosResultadoParaLivros(items))),
+    catchError((erro) => {
+      console.log(erro);
       return throwError(
         () =>
           new Error(
-            (this.mensagemErro = 'Ops, ocorreu um erro. Recaregue a aplicação.')
+            (this.mensagemErro = `Ops, ocorreu um erro! Recarregue a aplicação!`)
           )
       );
     })
