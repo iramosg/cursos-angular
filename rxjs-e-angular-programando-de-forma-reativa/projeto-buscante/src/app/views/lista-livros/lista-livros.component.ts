0256f28e2f99a5038dc4e9784edb31be913fafcd
Igor Ramos <igor.ramos@live.com>
2023-11-12 11:36:45 -0300

    Pipe async

--- rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-e-angular-programando-de-forma-reativa/projeto-buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Subscription, map, switchMap } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { map, switchMap, tap } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -10,40 +10,19 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: Livro[];
+export class ListaLivrosComponent {
   campoBusca = new FormControl();
-  subscription: Subscription;
-  livro: Livro;
 
   constructor(private service: LivroService) {}
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    tap(() => console.log('Fluxo inicial')),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map((result) => {
-      console.log('Requisições ao servidor');
-      this.listaLivros = this.livrosResultadoParaLivros(result);
-    })
+    tap(() => console.log('Requisição ao servidor')),
+    map((result) => this.livrosResultadoParaLivros(result))
   );
 
-  // buscarLivros() {
-  //   this.subscription = this.service.buscar(this.campoBusca).subscribe({
-  //     next: (result) => {
-  //       console.log('Requisições ao servidor');
-  //       this.listaLivros = this.livrosResultadoParaLivros(result);
-  //     },
-  //     error: (error) => console.log(error), // Se parar aqui, não executa o complete
-  //     complete: () => console.log('Observable completado.'),
-  //   });
-  // }
-
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
-    return items.map((item) => {
-      return new LivroVolumeInfo(item);
-    });
+    return items.map((item) => new LivroVolumeInfo(item));
   }
 }
